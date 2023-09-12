498a0c66e532987cac65c348d7c8d2f1a8712091
cpf <chenpengfei@aicaihr.com>
2023-09-12 13:55:27 +0800

    update

--- pm2.config.js
+++ pm2.config.js
@@ -1,15 +1,20 @@
 module.exports = {
     apps: {
-        "name": "note-server",                             // 项目名
+        "name": "blog-server",                             // 项目名
         "script": "./bin/www",                      // 执行文件
         "cwd": "./",                                // 根目录
         "args": "",                                 // 传递给脚本的参数
         "interpreter": "",                          // 指定的脚本解释器
         "interpreter_args": "",                     // 传递给解释器的参数
-        "watch": true,                              // 是否监听文件变动然后重启
+        "watch":[
+            "controller",
+            "database"
+        ],                              // 是否监听文件变动然后重启
         "ignore_watch": [                           // 不用监听的文件
             "node_modules",
-            "logs"
+            "logs",
+            'public',
+            "uploads"
         ],
         "exec_mode": "cluster_mode",                // 应用启动模式，支持fork和cluster模式
         "instances": 4,                             // 应用启动实例个数，仅在cluster模式有效 默认为fork；或者 max
@@ -25,7 +30,7 @@ module.exports = {
         "restart_delay": "60s",                     // 异常重启情况下，延时重启时间
         env: {
             NODE_ENV: "development",
-            PORT: 3000
+            PORT: 3300
         },
         env_test: {
             NODE_ENV: "test",
